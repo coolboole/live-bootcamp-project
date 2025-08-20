4aac774ecb0c722c393b64c77e1246e5c135dd15
coolboole <25207511+coolboole@users.noreply.github.com>
2025-08-20 20:30:09 +0100

    [Sprint 1][Task 5] test: adding test helpers

---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,78 @@
+use auth_service::Application;
+
+pub struct TestApp {
+    pub address: String,
+    pub http_client: reqwest::Client,
+}
+
+impl TestApp {
+    pub async fn new() -> Self {
+        let app = Application::build("127.0.0.1:0")
+            .await
+            .expect("Failed to build app");
+
+        let address = format!("http://{}", app.address.clone());
+
+        // Run the auth service in a separate async task
+        // to avoid blocking the main test thread. 
+        #[allow(clippy::let_underscore_future)]
+        let _ = tokio::spawn(app.run());
+
+        let http_client = reqwest::Client::builder(); // Create a Reqwest http client instance
+
+        // Create new `TestApp` instance and return it
+        return Self {
+            address,
+            http_client: http_client.build().expect("Failed to build HTTP client"),
+        };
+    }
+
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn get_signup(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/signup", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn get_login(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/login", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn get_logout(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/logout", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn get_verify_2fa(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/verify-2fa", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn get_verify_token(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/verify-token", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+}
