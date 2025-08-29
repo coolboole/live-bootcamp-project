b4dda64e3278d0c0ea8874fad4644fb8a07858b4
coolboole <25207511+coolboole@users.noreply.github.com>
2025-08-29 18:17:22 +0100

    [Sprint 2][Task 2] feat: adding AuthAPIError for signup

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,14 @@
-use axum::{Router, serve::Serve, routing::post};
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::post,
+    serve::Serve,
+    Json, Router,
+};
 use tower_http::services::ServeDir;
 use std::error::Error;
+use domain::AuthAPIError;
+use serde::{Deserialize, Serialize};
 use app_state::AppState;
 use routes::{login, logout, signup, verify_2fa, verify_token};
 
@@ -44,4 +52,25 @@ impl Application {
         self.server.await
     }
 
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ErrorResponse {
+    pub error: String,
+}
+
+impl IntoResponse for AuthAPIError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthAPIError::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists"),
+            AuthAPIError::InvalidCredentials => (StatusCode::BAD_REQUEST, "Invalid credentials"),
+            AuthAPIError::UnexpectedError => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected error")
+            }
+        };
+        let body = Json(ErrorResponse {
+            error: error_message.to_string(),
+        });
+        (status, body).into_response()
+    }
 }
