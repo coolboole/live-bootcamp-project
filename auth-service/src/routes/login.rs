64d9a9bed4bb5749ad6ae011870c9fdf7a169463
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-01 21:58:31 +0100

    [Sprint 3][Task 1] feat: implementing login functionality

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,39 @@
-use axum::{response::IntoResponse, http::StatusCode};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, User, email::Email, password::Password},
+};
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+pub async fn login(
+    state: State<AppState>,
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = 
+        Email::parse(request.email.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+    let password = 
+        Password::parse(request.password.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+
+    let user = User::new(email, password, false);
+
+    let user_store = state.user_store.read().await;
+
+    user_store.validate_user(&user.email, &user.password).await.map_err(|_| AuthAPIError::UnexpectedError)?;
+
+    let response = Json(LoginResponse {
+        message: "Login successful!".to_string(),
+    });
+
+    Ok((StatusCode::OK, response))
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct LoginResponse {
+    pub message: String,
 }
