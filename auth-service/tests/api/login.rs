62488570808d0aa84a3649ffbc217fccb7c3316b
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-01 21:10:12 +0100

    [Sprint 3][Task 1] test: check if malformed credentials to login return 422

--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,10 +1,27 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn login_returns_200() {
+async fn should_return_422_if_malformed_credentials() {
     let app = TestApp::new().await;
 
-    let response = app.post_login().await;
+    let random_email = get_random_email();
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+        }),
+        serde_json::json!({
+            "email": &random_email,
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = TestApp::post_login(&app, test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
