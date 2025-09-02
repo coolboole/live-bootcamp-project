04c1ce469180eb77dee97df4f7543bfae5bb0a16
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-02 18:05:09 +0100

    [Sprint 3][Task 1] test: invalid credentials return 400

--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,4 +1,5 @@
 use crate::helpers::{get_random_email, TestApp};
+use auth_service::ErrorResponse;
 
 #[tokio::test]
 async fn should_return_422_if_malformed_credentials() {
@@ -23,5 +24,37 @@ async fn should_return_422_if_malformed_credentials() {
             "Failed for input: {:?}",
             test_case
         );
+    }
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    // Call the log-in route with invalid credentials and assert that a
+    // 400 HTTP status code is returned along with the appropriate error message. 
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let test_cases = [
+        serde_json::json!({
+            "email": "",
+            "password": "password123",
+        }),
+        serde_json::json!({
+            "email": &random_email,
+            "password": "",
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = TestApp::post_login(&app, test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for input: {:?}",
+            test_case
+        );
+        let error_response: ErrorResponse = response.json().await.unwrap();
+        assert_eq!(error_response.error, "Invalid credentials");
     }
 }
