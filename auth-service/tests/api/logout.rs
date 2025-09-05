edabb603d1aa2da1dcc675f5f2c9f2a97fcd2c78
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-05 23:40:35 +0100

    [Sprint 3][Task 3] test: ensuring logout returns 200 if returning valid JWT and 400 if logging out twice

--- auth-service/tests/api/logout.rs
+++ auth-service/tests/api/logout.rs
@@ -1,7 +1,7 @@
 use auth_service::{utils::constants::JWT_COOKIE_NAME, ErrorResponse};
-use reqwest::Url;
+use reqwest::{cookie::CookieStore, Url};
 
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
 async fn should_return_400_if_jwt_cookie_missing() {
@@ -34,4 +34,73 @@ async fn should_return_401_if_invalid_token() {
 
     let response = app.post_logout().await;
     assert_eq!(response.status().as_u16(), 401);
+}
+
+#[tokio::test]
+async fn should_return_200_if_valid_jwt_cookie() {
+    let app = TestApp::new().await;
+
+    let email = get_random_email();
+    let password = "BAD_Pa$$w0rd!";
+
+    // register user
+    app.post_signup(
+        &serde_json::json!({
+            "email": &email,
+            "password": password,
+            "requires2FA": false
+        }),
+    ).await;
+
+    // login user
+    app.post_login(
+        &serde_json::json!({
+            "email": &email,
+            "password": password
+        }),
+    ).await;
+
+    // ensure cookie is set
+    assert!(app.cookie_jar.cookies(&Url::parse(&app.address).unwrap()).is_some());
+
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn should_return_400_if_logout_called_twice_in_a_row() {
+    let app = TestApp::new().await;
+
+    let email = get_random_email();
+    let password = "BAD_Pa$$w0rd!";
+
+    // register user
+    app.post_signup(
+        &serde_json::json!({
+            "email": &email,
+            "password": password,
+            "requires2FA": false
+        }),
+    ).await;
+
+    // login user
+    app.post_login(
+        &serde_json::json!({
+            "email": &email,
+            "password": password
+        }),
+    ).await;
+
+    // ensure cookie is set
+    assert!(app.cookie_jar.cookies(&Url::parse(&app.address).unwrap()).is_some());
+
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), 200);
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), 400);
+    let response_body: ErrorResponse = response
+        .json()
+        .await
+        .expect("Failed to parse response body as JSON");
+    assert_eq!(response_body.error, "Missing token");
 }
