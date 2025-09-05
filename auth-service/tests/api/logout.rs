7b0ce1b4c88e03abee8f20edf9a5655afbe72824
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-05 23:10:38 +0100

    [Sprint 3][Task 3] fix: correcting application address and disabling secure request in logout 401 test

--- auth-service/tests/api/logout.rs
+++ auth-service/tests/api/logout.rs
@@ -26,10 +26,10 @@ async fn should_return_401_if_invalid_token() {
     // add invalid cookie
     app.cookie_jar.add_cookie_str(
         &format!(
-            "{}=invalid; HttpOnly; SameSite=Lax; Secure; Path=/",
+            "{}=invalid; HttpOnly; SameSite=Lax; Path=/",
             JWT_COOKIE_NAME
         ),
-        &Url::parse("http://127.0.0.1").expect("Failed to parse URL"),
+        &Url::parse(&app.address).expect("Failed to parse URL"),
     );
 
     let response = app.post_logout().await;
