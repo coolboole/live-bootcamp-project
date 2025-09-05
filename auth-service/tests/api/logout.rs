c53ef7fbff7afab1f37070370f4432bd3a9a92a3
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-05 22:37:18 +0100

    [Sprint 3][Task 3] test: checking that logout returns 400 if cookie is missing and 401 if token is invalid

--- auth-service/tests/api/logout.rs
+++ auth-service/tests/api/logout.rs
@@ -1,10 +1,37 @@
+use auth_service::{utils::constants::JWT_COOKIE_NAME, ErrorResponse};
+use reqwest::Url;
+
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn logout_returns_200() {
+async fn should_return_400_if_jwt_cookie_missing() {
     let app = TestApp::new().await;
 
     let response = app.post_logout().await;
 
-    assert_eq!(response.status().as_u16(), 200);
+    assert_eq!(response.status().as_u16(), 400);
+
+    let response_body: ErrorResponse = response
+        .json()
+        .await
+        .expect("Failed to parse response body as JSON");
+
+    assert_eq!(response_body.error, "Missing token");
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+
+    // add invalid cookie
+    app.cookie_jar.add_cookie_str(
+        &format!(
+            "{}=invalid; HttpOnly; SameSite=Lax; Secure; Path=/",
+            JWT_COOKIE_NAME
+        ),
+        &Url::parse("http://127.0.0.1").expect("Failed to parse URL"),
+    );
+
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), 401);
 }
