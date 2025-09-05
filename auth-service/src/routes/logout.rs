fcd582caeb536c6f9bc07b71a882ca0faac18749
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-05 22:46:58 +0100

    [Sprint 3][Task 3] feat: creating logout route

--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -1,5 +1,28 @@
-use axum::{response::IntoResponse, http::StatusCode};
+use axum::{http::StatusCode, response::IntoResponse};
+use axum_extra::extract::CookieJar;
 
-pub async fn logout() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{
+    domain::AuthAPIError,
+    utils::{auth::validate_token, constants::JWT_COOKIE_NAME},
+};
+
+pub async fn logout(jar: CookieJar) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
+    // Retrieve JWT cookie from the `CookieJar`
+    // Return AuthAPIError::MissingToken is the cookie is not found
+    let cookie = match jar.get(JWT_COOKIE_NAME) {
+        Some(cookie) => cookie,
+        None => return (jar, Err(AuthAPIError::MissingToken)),
+    };
+
+    let token = cookie.value().to_owned();
+
+    // Validate JWT token by calling `validate_token` from the auth service.
+    // If the token is valid you can ignore the returned claims for now.
+    // Return AuthAPIError::InvalidToken is validation fails.
+    match validate_token(&token).await {
+        Ok(_claims) => {},
+        Err(_) => return (jar, Err(AuthAPIError::InvalidToken)),
+    };
+
+    (jar, Ok(StatusCode::OK))
 }
