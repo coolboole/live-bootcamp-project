6b4d74192667c6fc3a8cedcbc14b07214c5ddf90
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-06 11:53:19 +0100

    [Sprint 3][Task 3] test: checking 200 should return after valid token and 401 after invalid token

--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -1,4 +1,6 @@
-use crate::helpers::TestApp;
+use auth_service::{utils::constants::JWT_COOKIE_NAME, ErrorResponse};
+
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
 async fn should_return_422_if_malformed_input() {
@@ -6,4 +8,64 @@ async fn should_return_422_if_malformed_input() {
 
     let response = app.post_verify_token(&serde_json::json!({})).await;
     assert_eq!(response.status().as_u16(), 422);
+}
+
+#[tokio::test]
+async fn should_return_200_valid_token() {
+    let app = TestApp::new().await;
+
+    let email = get_random_email();
+    let password = "BAD_Pa$$w0rd!";
+
+    let signup_body = serde_json::json!({
+        "email": email,
+        "password": password,
+        "requires2FA": false
+    });
+
+    let response = app.post_signup(&signup_body).await;
+    assert_eq!(response.status().as_u16(), 201);
+
+    let login_body = serde_json::json!({
+        "email": email,
+        "password": password
+    });
+
+    let response = app.post_login(&login_body).await;
+    assert_eq!(response.status().as_u16(), 200);
+
+    let cookie = response
+        .cookies()
+        .find(|c| c.name() == JWT_COOKIE_NAME)
+        .expect("No auth cookie found");
+
+    let token = cookie.value();
+
+    let verify_token_body = serde_json::json!({
+        "token": &token
+    });
+
+    let response = app.post_verify_token(&verify_token_body).await;
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+
+    let verify_token_body = serde_json::json!({
+        "token": "invalid_token"
+    });
+
+    let response = app.post_verify_token(&verify_token_body).await;
+    assert_eq!(response.status().as_u16(), 401);
+
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "Invalid token".to_owned()
+    );
 }
