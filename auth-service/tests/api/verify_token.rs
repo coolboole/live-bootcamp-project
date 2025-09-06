56c96497d7eb008c7bca2f1fea7df195c0cdaff8
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-06 11:28:07 +0100

    [Sprint 3][Task 3] test: parsing body into post_verify_token and testing for 422 for malformed input

--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -1,10 +1,9 @@
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn verify_token_returns_200() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.post_verify_token().await;
-
-    assert_eq!(response.status().as_u16(), 200);
+    let response = app.post_verify_token(&serde_json::json!({})).await;
+    assert_eq!(response.status().as_u16(), 422);
 }
