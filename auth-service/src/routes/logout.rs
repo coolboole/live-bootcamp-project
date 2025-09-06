9a81f8cebeb71733a83f54e2c095dd57ba23b3e3
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-06 10:43:08 +0100

    [Sprint 3][Task 3] feat: remove cookie after logout

--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -1,5 +1,5 @@
 use axum::{http::StatusCode, response::IntoResponse};
-use axum_extra::extract::CookieJar;
+use axum_extra::extract::{cookie, CookieJar};
 
 use crate::{
     domain::AuthAPIError,
@@ -23,6 +23,8 @@ pub async fn logout(jar: CookieJar) -> (CookieJar, Result<impl IntoResponse, Aut
         Ok(_claims) => {},
         Err(_) => return (jar, Err(AuthAPIError::InvalidToken)),
     };
+
+    let jar = jar.remove(cookie::Cookie::from(JWT_COOKIE_NAME));
 
     (jar, Ok(StatusCode::OK))
 }
