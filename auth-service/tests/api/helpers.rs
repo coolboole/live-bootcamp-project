9a91f7c9f0db7b2cc589966987133c1fc61ad764
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-06 20:07:02 +0100

    [Sprint 3][Task 5] feat: adding HashsetBannedTokenStore to prod and test AppState

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -1,5 +1,10 @@
 use auth_service::{
-    app_state::AppState, services::hashmap_user_store::HashmapUserStore, utils::constants::test,
+    app_state::AppState, 
+    services::{
+        hashmap_user_store::HashmapUserStore,
+        hashset_banned_token_store::HashsetBannedTokenStore
+    }, 
+    utils::constants::test,
     Application,
 };
 use reqwest::cookie::Jar;
@@ -15,7 +20,11 @@ pub struct TestApp {
 impl TestApp {
     pub async fn new() -> Self {
         let user_store = HashmapUserStore::default();
-        let app_state = AppState::new(std::sync::Arc::new(tokio::sync::RwLock::new(user_store)));
+        let banned_token_store = HashsetBannedTokenStore::default();
+        let app_state = AppState::new(
+            std::sync::Arc::new(tokio::sync::RwLock::new(user_store)),
+            std::sync::Arc::new(tokio::sync::RwLock::new(banned_token_store)),
+        );
 
         let app = Application::build(app_state, test::APP_ADDRESS)
             .await
