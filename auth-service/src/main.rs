27bf8f4969aa40c1546d48a8de394208751d3e62
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-11 18:46:40 +0100

    [Sprint 4][Task 1] feat: implementing TwoFACodeStore into live AppState

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,7 +1,8 @@
 use auth_service::{
     app_state::AppState, services::{
         hashmap_user_store::HashmapUserStore, 
-        hashset_banned_token_store::HashsetBannedTokenStore
+        hashset_banned_token_store::HashsetBannedTokenStore,
+        hashmap_two_fa_code_store::HashmapTwoFACodeStore
     }, 
     utils::constants::prod,
     Application,
@@ -11,9 +12,11 @@ use auth_service::{
 async fn main() {
     let user_store = HashmapUserStore::default();
     let banned_token_store = HashsetBannedTokenStore::default();
+    let two_fa_code_store = HashmapTwoFACodeStore::default();
     let app_state = AppState::new(
         std::sync::Arc::new(tokio::sync::RwLock::new(user_store)),
         std::sync::Arc::new(tokio::sync::RwLock::new(banned_token_store)),
+        std::sync::Arc::new(tokio::sync::RwLock::new(two_fa_code_store)),
     );
 
     let app = Application::build(app_state, prod::APP_ADDRESS)
