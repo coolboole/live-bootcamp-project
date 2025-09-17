e2a39ed0a697c2529696b71156a3726f4f553002
coolboole <25207511+coolboole@users.noreply.github.com>
2025-09-17 20:22:09 +0100

    [Sprint 4][Task 1] feat: implementing LoginAttemptId for login

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -3,64 +3,71 @@ use axum_extra::extract::CookieJar;
 use serde::{Deserialize, Serialize};
 use crate::{
     app_state::AppState,
-    domain::{AuthAPIError, UserStoreError, email::Email, password::Password},
+    domain::{AuthAPIError, Email, LoginAttemptId, Password, TwoFACode},
     utils::auth::generate_auth_cookie,
 };
 
 pub async fn login(
-    state: State<AppState>,
+    State(state): State<AppState>,
     jar: CookieJar,
     Json(request): Json<LoginRequest>,
 ) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
-    let email = match Email::parse(request.email.clone()) {
-        Ok(email) => email,
-        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
-    };
-    let password = match Password::parse(request.password.clone()){
+    let password = match Password::parse(request.password) {
         Ok(password) => password,
         Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
     };
 
-    let user_store = state.user_store.read().await;
-
-    match user_store.validate_user(&email, &password).await {
-        Ok(()) => {},
-        Err(UserStoreError::UserNotFound) => {
-            return (jar, Err(AuthAPIError::IncorrectCredentials));
-        },
-        Err(UserStoreError::InvalidCredentials) => {
-            return (jar, Err(AuthAPIError::IncorrectCredentials));
-        },
-        Err(_) => {
-            return (jar, Err(AuthAPIError::UnexpectedError));
-        }
+    let email = match Email::parse(request.email) {
+        Ok(email) => email,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
     };
+
+    let user_store = &state.user_store.read().await;
+
+    if user_store.validate_user(&email, &password).await.is_err() {
+        return (jar, Err(AuthAPIError::IncorrectCredentials));
+    }
 
     let user = match user_store.get_user(&email).await {
         Ok(user) => user,
         Err(_) => return (jar, Err(AuthAPIError::IncorrectCredentials)),
     };
 
-    // Handle request based on user's 2FA configuration
     match user.requires_2fa {
-        true => handle_2fa(jar).await,
+        true => handle_2fa(&user.email, &state, jar).await,
         false => handle_no_2fa(&user.email, jar).await,
     }
-
-    // (updated_jar, Ok(StatusCode::OK.into_response()))
 }
 
 async fn handle_2fa(
+    email: &Email,
+    state: &AppState,
     jar: CookieJar,
 ) -> (
     CookieJar,
     Result<(StatusCode, Json<LoginResponse>), AuthAPIError>,
 ) {
+    // First, we must generate a new random login attempt ID and 2FA code
+    let login_attempt_id = LoginAttemptId::default();
+    let two_fa_code = TwoFACode::default();
+
+    // Store the ID and code in our 2FA code store. Return `AuthAPIError::UnexpectedError` if the operation fails
+    if state
+        .two_fa_code_store
+        .write()
+        .await
+        .add_code(email.clone(), login_attempt_id.clone(), two_fa_code.clone())
+        .await
+        .is_err()
+    {
+        return (jar, Err(AuthAPIError::UnexpectedError));
+    }
+
     // Return a TwoFactorAuthResponse. The message should be "2FA required".
     // The login attempt ID should be "123456". We will replace this hard-coded login attempt ID soon!
     let response = LoginResponse::TwoFactorAuth(TwoFactorAuthResponse {
         message: "2FA required".to_string(),
-        login_attempt_id: "123456".to_string(),
+        login_attempt_id: login_attempt_id.as_ref().to_owned(),
     });
 
     (jar, Ok((StatusCode::PARTIAL_CONTENT, Json(response))))
